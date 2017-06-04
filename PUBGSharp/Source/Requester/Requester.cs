60706076c31eabf220e3851ca8dbe52ae2bb0fe0
Alex <dwt416@gmail.com>
2017-06-04 21:48:38 +0300

    Added more error handling

--- PUBGSharp/Source/Requester/Requester.cs
+++ PUBGSharp/Source/Requester/Requester.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -17,10 +18,23 @@ namespace PUBGSharp
         {
             using (HttpClient http = new HttpClient())
             {
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://pubgtracker.com/api/profile/pc/{playerName}?region=agg");
-                request.Headers.Add("TRN-Api-Key", ApiKey);
-                HttpResponseMessage response = await http.SendAsync(request);
-                return JsonConvert.DeserializeObject<StatsResponse>(await response.Content.ReadAsStringAsync());
+                StatsResponse result = new StatsResponse();
+                try
+                {
+                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://pubgtracker.com/api/profile/pc/{playerName}?region=agg");
+                    request.Headers.Add("TRN-Api-Key", ApiKey);
+                    HttpResponseMessage response = await http.SendAsync(request);
+                    if (!response.IsSuccessStatusCode) throw new HttpRequestException("Could not retrieve stats.");
+                    string t = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<StatsResponse>(t);
+                    if (result.AccountId == null) throw new JsonException("Player data is not valid. Player might not exist, or their stats haven't been updated yet.");
+                    Console.WriteLine(t);
+                }
+                catch (JsonException)
+                {
+                    throw new JsonException("Failed to deserialize data. Player might not exist or have stats in specified region or gamemode.");
+                }
+                return result;
             }
         }
     }
