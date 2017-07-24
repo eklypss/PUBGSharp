09dca55fea2e9ad0feb7ed118ec1d1d4568eb251
eklypss <eklypssdev@gmail.com>
2017-07-24 23:32:53 +0300

    Removed unnecessary usings

--- PUBGSharp/Net/Model/MatchHistoryStat.cs
+++ PUBGSharp/Net/Model/MatchHistoryStat.cs
@@ -1,11 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using PUBGSharp.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PUBGSharp.Net.Model
 {
