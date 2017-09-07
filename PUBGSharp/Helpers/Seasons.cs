baed5449b29ebacd504fa2a86734725a4614f9a8
eklypss <eklypssdev@gmail.com>
2017-09-07 22:48:46 +0300

    Add support for season 4 & future seasons

--- PUBGSharp/Helpers/Seasons.cs
+++ PUBGSharp/Helpers/Seasons.cs
@@ -8,5 +8,9 @@
         public const string EASeason1 = "2017-pre1";
         public const string EASeason2 = "2017-pre2";
         public const string EASeason3 = "2017-pre3";
+        public const string EASeason4 = "2017-pre4";
+        public const string EASeason5 = "2017-pre5";
+        public const string EASeason6 = "2017-pre6";
+        public const string EASeason7 = "2017-pre7";
     }
 }
